8e3cb96b85d9a457ffc0f9c3b9d2951375fc4d00
Radu1990 <et2323@gmail.com>
2021-04-09 18:03:11 +0200

    remove Dockerfile(stretch image)

--- buildImage.bash
+++ buildImage.bash
@@ -1,1 +1,10 @@
-docker build -t restorecommerce/ordering-srv .
+#!/bin/bash
+
+SERVICE_NAME="ordering-srv"
+
+DOCKER_BUILDKIT=1 docker build \
+  --tag restorecommerce/$SERVICE_NAME \
+  -f ./Dockerfile \
+  --cache-from restorecommerce/$SERVICE_NAME \
+  --build-arg APP_HOME=/home/node/$SERVICE_NAME \
+  .
