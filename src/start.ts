888d2c05088059eda1d2d000ce6e24e71018eafa
Gerald Baulig <gerald.baulig@n-fuse.co>
2023-09-04 13:34:58 +0200

    fix(redis): upgrade redis configs

--- src/start.ts
+++ src/start.ts
@@ -1,8 +1,8 @@
-import { ClusterService as Cluster } from '@restorecommerce/cluster-service';
+import ClusterService from '@restorecommerce/cluster-service';
 import { createServiceConfig } from '@restorecommerce/service-config';
 
 const cfg = createServiceConfig(process.cwd());
-const server = new Cluster(cfg);
+const server = new ClusterService(cfg);
 server.run('./lib/worker');
 process.on('SIGINT', () => {
   server.stop();
